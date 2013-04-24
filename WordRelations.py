1650e64765210fa21ea06a9c73f4ebbd6b3778e0
Benjamin Slawski <bslawski01@gmail.com>
2013-04-24 17:59:57 -0400

    added template structure

--- WordRelations.py
+++ WordRelations.py
@@ -1,2 +1,5 @@
 def WordRelations(wordlist):
+
+    ## TODO: implement this method
+
     return {}, map(lambda k: {"NAME__":k}, wordlist)
--- app.py
+++ app.py
@@ -28,8 +28,7 @@ app.config['SECRET_KEY'] = 'VKqerq3E/adf23d0444'
 AUTH_LEVEL = 4
 
 
-@app.route('/', methods=['POST', 'GET'])
-def show_bucket():
+def checkAuth():
     token = request.args.get('token')
     if not token:
         try:
@@ -43,33 +42,31 @@ def show_bucket():
         return redirect('http://data-dashboard.herokuapp.com')
 
     updateSession(session['access_token'], session['auth_level'])
+
+
+@app.route('/', methods=['POST', 'GET'])
+def show_bucket():
+##    checkAuth()
 
     bucket_list = getBuckets()
 
     if request.method == 'GET':
         selected_bucket = choice(bucket_list)
 
-    bucket_stats, bucket_contents = loadBucket(selected_bucket)
+##    bucket_stats, bucket_contents = loadBucket(selected_bucket)
 
-    return json.dumps([bucket_stats, bucket_contents])
-##    return render_template('show_bucket.html', selected_bucket=bucket_stats, bucket_list=bucket_list, bucket_contents=bucket_contents)
+    bucket_contents = [{'NAME__':'science', 'CENTRALITY__':.1, 'sciences':.05, 'technology':.21}, \
+                       {'NAME__':'sciences', 'CENTRALITY__':.11, 'science':.05, 'technology':.19}, \
+                       {'NAME__':'technology', 'CENTRALITY__':.29, 'science':.21, 'sciences':.19}]
+    bucket_stats = {'NAME__':'Science/Technology', 'REACH__':3511000, 'COHESION__':.87, 'SPREAD__':2.31, 'SIZE__':len(bucket_contents)}
+
+##    return json.dumps([bucket_stats, bucket_contents])
+    return render_template('show_bucket.html', selected_bucket=bucket_stats, bucket_list=bucket_list, bucket_contents=bucket_contents)
 
 
 @app.route('/save/', methods=['POST'])
 def save_bucket():
-    token = request.args.get('token')
-    if not token:
-        try:
-            token = session['access_token']
-        except KeyError:
-            return redirect('http://data-dashboard.herokuapp.com')
-
-    session['access_token'] = str(token)
-    session['auth_level'] = getAccessLevel(session['access_token'])
-    if session['auth_level'] < AUTH_LEVEL:
-        return redirect('http://data-dashboard.herokuapp.com')
-
-    updateSession(session['access_token'], session['auth_level'])
+##    checkAuth()
 
     ## TODO: implement this method
     return
