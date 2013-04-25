088dd26fcae95a639895c6e86e346a59de080a7e
Benjamin Slawski <bslawski01@gmail.com>
2013-04-25 18:33:01 -0400

    doesn't re-create lin object on each function call

--- semantic_dist.pl
+++ semantic_dist.pl
@@ -4,10 +4,10 @@ use WordNet::Similarity::lin;
 my $word_net = WordNet::QueryData->new();
 die "Failure creating WordNet Object\n" if (!$word_net);
 
-sub semantic_dist {
-    my $lin = WordNet::Similarity::lin-> new($word_net);
-    die "Unable to create Lin semantic dist analyzer\n" if (!defined $lin);
+my $lin = WordNet::Similarity::lin-> new($word_net);
+die "Unable to create Lin semantic dist analyzer\n" if (!defined $lin);
 
+sub semantic_dist {
     print $_[0] . " - " . $_[1];
     $similarity = $lin->getRelatedness($_[0], $_[1]);
     ($error, $errString) = $lin->getError();
