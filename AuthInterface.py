720e6cf816c569350a3eaa189e1b05a5700efc70
Benjamin Slawski <bslawski01@gmail.com>
2013-06-05 12:53:44 -0700

    commented auth, neo4j and stats interfaces

--- AuthInterface.py
+++ AuthInterface.py
@@ -3,6 +3,8 @@ from storm.locals import *
 from time import *
 
 
+## connects to the authentication postgresql database,
+## returning a storm Store as a handle to the database
 def getDBhandle():
     dbname = 'postgres://jytokmqhvhruvs:sEFZxUgkFc02iEogBgDJzuT2t4@ec2-23-21-209-205.compute-1.amazonaws.com:5432/ddetrgamclnu77'
 
@@ -17,6 +19,9 @@ def getDBhandle():
     return store
 
 
+## given an authentication token, looks up that token in the 
+## authentication database, returning the authentication level
+## of that token
 def getAccessLevel(token):
     db = getDBhandle()
 
@@ -38,6 +43,9 @@ def getAccessLevel(token):
     return results[0][0]
 
 
+## given an authentication token and authentication level, 
+## sets the given token to the given auth level, as well
+## as resetting that token's auto-logout time
 def updateSession(token, level, handles='', logout_time=900):
     db = getDBhandle()
 
--- StatsInterface.py
+++ StatsInterface.py
@@ -4,6 +4,8 @@ from storm.locals import *
 from time import sleep
 
 
+## connects to the bucket stats postgresql database,
+## returning a storm Store as a handle to the database
 def getDBhandle():
     dbname = 'postgres://fifmkeftvutysz:gUuDxC3K9LhKB2koOCLVpIit7e@ec2-54-235-155-182.compute-1.amazonaws.com:5432/d41qkgihdppdi4'
 
@@ -19,6 +21,7 @@ def getDBhandle():
     return store
 
 
+## executes the given query on the given database handle
 def make_query(db, query):
     while True:
         try:
@@ -30,15 +33,24 @@ def make_query(db, query):
             sleep(.25)
 
 
+## gets multiple stats for a single bucket from the
+## buckets stats database
 class StatsFetcher:
 
+    ## constructor
+    ## bucket key - hash key of the bucket we want stats from
+    ## stats - list of stats to lookup
+    ##         possible vals: ['CPC', 'CTR', 'CPM']
     def __init__(self, bucket_key, stats):
         self.bucket_key = bucket_key
         self.stats = stats
 
+        ## holds stat data
         self.data = {}
+        ## list of worker threads
         self.workers = []
 
+        ## starts StatThread workers to look up stats
         if bucket_key and stats:
             self.db = getDBhandle()
     
@@ -48,15 +60,23 @@ class StatsFetcher:
                 new_worker.start()
 
 
+    ## returns stat data when workers are finished
     def getStats(self):
         map(lambda k: k.join(), self.workers)
         self.workers = []
         return self.data
 
 
-
+## worker thread for looking up a single stat for a single bucket
+## from the bucket stats database
 class StatThread(Thread):
 
+    ## constructor
+    ## stat - name of the stat to look up
+    ##        possible vals: ['CPC', 'CTR', 'CPM']
+    ## bucket_key - hash key of the bucket to look up
+    ## db - storm.Store handle of the bucket stats db
+    ## data - common dict for storing results
     def __init__(self, stat, bucket_key, db, data):
         Thread.__init__(self)
 
@@ -66,27 +86,33 @@ class StatThread(Thread):
         self.data = data
 
 
+    ## looks up a stat from the bucket stats database
     def run(self):
+        ## if the stat is not valid, will find no results
         result = None
 
+        ## looks up click through rate
         if self.stat == 'CTR':
             query = 'SELECT SUM(clicks)/SUM(impressions) ' \
                   + 'FROM bucket_stats ' \
                   + 'WHERE keywords_hash = \'' + self.bucket_key + '\';'
             result = make_query(self.db, query)
 
+        ## looks up cost per click
         elif self.stat == 'CPC':
             query = 'SELECT SUM(spent)/SUM(clicks) ' \
                   + 'FROM bucket_stats ' \
                   + 'WHERE keywords_hash = \'' + self.bucket_key + '\';'
             result = make_query(self.db, query)
 
+        ## looks up cost per 1k impressions
         elif self.stat == 'CPM':
             query = 'SELECT 1000*SUM(spent)/SUM(impressions) ' \
                   + 'FROM bucket_stats ' \
                   + 'WHERE keywords_hash = \'' + self.bucket_key + '\';'
             result = make_query(self.db, query)
 
+        ## if a result was found, stores it in the common data dict
         if result and result[0] and result[0][0]:
             self.data[self.stat] = result[0][0]
        
